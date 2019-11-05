d398861efe70cc962410d872cc84a78ba57ccd84
Ruiqi Song <rs927@cornell.edu>
2019-11-04 19:02:44 -0500

    sig max health added

--- player.ml
+++ player.ml
@@ -50,6 +50,8 @@ module type P = sig
        experience value for that level alone, and [Illegal] otherwise.*)
   val advance_level : t -> result 
 
+  val max_health : int
+
 end 
 
 module Player = struct
